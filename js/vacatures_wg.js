289a901f21cbf2c7347526a81c80cd29be366be5
Stevne <steven.de.roey@student.ehb.be>
2015-05-20 13:38:03 +0200

    bugfix 1
    done

--- js/vacatures_wg.js
+++ js/vacatures_wg.js
@@ -12,23 +12,34 @@ $(function(){
     $listItems.on("swipeleft", function(e){
         console.log('swipeleft');
         
-        // RESET ALL ORIGINAL POSITIONS
-        $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
-        $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
-        $listItemIcons.animate({opacity:'0'},100,'linear');
+        var alreadyOpenTest = $(this).parent().find('.sliderOnTop').css('width');
+        alreadyOpenTest = alreadyOpenTest.substring(0,alreadyOpenTest.length-2);
+        if(alreadyOpenTest>0){
+            //nothing happens
+        }
+        else{
+            // RESET ALL ORIGINAL POSITIONS
+            $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
+            $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
+            $listItemIcons.animate({opacity:'0'},100,'linear');
+
+            // MOVE SWIPED ITEM
+            $listItemIcons.animate({opacity:'1'},100,'linear');
+            $(this).parent().find('.sliderUnder .text').animate({margin:'0 0 0 -15%'},400,'easeOutBounce');
+            $(this).parent().find('.sliderOnTop').animate({width: '40%'},400,'easeOutBounce');
+        }
         
-        // MOVE SWIPED ITEM
-        $listItemIcons.animate({opacity:'1'},100,'linear');
-        $(this).parent().find('.sliderUnder .text').animate({margin:'0 0 0 -15%'},400,'easeOutBounce');
-        $(this).parent().find('.sliderOnTop').animate({width: '40%'},400,'easeOutBounce');
     });
     
     //SWIPE RIGHT
     $listItems.on("swiperight", function(e){
-        $listItemIcons.animate({opacity:'0'},100,'linear');
-        $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
-        $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
-        
+        var alreadyOpenTest = $(this).parent().find('.sliderOnTop').css('width');
+        alreadyOpenTest = alreadyOpenTest.substring(0,alreadyOpenTest.length-2);
+        if(alreadyOpenTest>0){
+            $listItemIcons.animate({opacity:'0'},100,'linear');
+            $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
+            $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
+        }
     });
     
 });
