558ce4babb201d3d04e6b8c7592f6d8f1ec1db84
Stevne <steven.de.roey@student.ehb.be>
2015-05-20 12:01:09 +0200

    woah 2.0
    now with more woah

--- js/vacatures_wg.js
+++ js/vacatures_wg.js
@@ -4,23 +4,23 @@ $(function(){
     
     var $listItems = $('#list .sliderUnder');
     var $listItemsSwiped = $('#list .sliderOnTop');
+    var $listItemsText = $('#list .sliderUnder .text');
     
     $listItems.on("swipeleft", function(e){
         console.log('swipeleft');
-        $listItemsSwiped.animate({
-            width: '0'
-        },200,'easeInOutQuad');
-        $(this).parent().find('.sliderOnTop').animate({
-            width: '40%'
-        },600,'easeOutBounce');
+        
+        // RESET ALL ORIGINAL POSITIONS
+        $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
+        $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
+        
+        //MOVE SWIPED ITEM
+        $(this).parent().find('.sliderUnder .text').animate({margin:'0 0 0 -15%'},400,'easeOutBounce');
+        $(this).parent().find('.sliderOnTop').animate({width: '40%'},400,'easeOutBounce');
     });
     $listItems.on("swiperight", function(e){
-        $listItemsSwiped.animate({
-            width: '0'
-        },200,'easeInOutQuad');
+        $listItemsText.animate({margin:'0 0 0 5%'},200,'easeInOutQuad');
+        $listItemsSwiped.animate({width:'0'},200,'easeInOutQuad');
+        
     });
-    
-    
-    
     
 });
