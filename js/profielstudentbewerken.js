cec06a1a0ec6559e0703d7cb5111f9e55532f1ad
Mcalewaert <michael.calewaert@gmail.com>
2015-05-21 15:23:08 +0200

    Bewerken studenten Profiel

--- js/profielstudentbewerken.js
+++ js/profielstudentbewerken.js
@@ -4,20 +4,35 @@ browser: true,
 jquery: true
 */
 
-$(document).ready(function(){
-    $(".bewerkinformatie").click(function(){
-    $(this).parent().next(".beschrijving").children().replaceWith("<form><input class='invoerveld' type='text' autofocus placeholder='test'></form>")
-    console.log(this);
-$(".invoerveld").blur(function(){
-    var waarde = $(".invoerveld").val();
-    console.log(waarde);
-    $(".invoerveld").replaceWith("<p class='details'>" + waarde + "</p>");
-    
-})
-    });
-    
+$(document).ready(function () {
+    $(".bewerkinformatie").click(function () {
+        $(this).parent().next(".beschrijving").children().replaceWith("<form><input class='invoerveld' type='text'></form>");
+        $('input').focus();
+        $(".invoerveld").blur(function(){
+                  var waarde = $(".invoerveld").val();  
+                    console.log(waarde);
+        $(this).replaceWith("<p class='details'>" + waarde + "</p>");     
+        });
 
+});
+        $(".bewerkinformatiedatum").click(function () {
+        $(this).parent().next(".beschrijving").children().replaceWith("<form><input class='invoerveld' type='date'></form>");
+        $('input').focus();
+        $(".invoerveld").blur(function(){
+                  var waarde = $(".invoerveld").val();  
+                    console.log(waarde);
+        $(this).replaceWith("<p class='details'>" + waarde + "</p>");     
+        });
 
+});
+            $(".bewerkinformatieemail").click(function () {
+        $(this).parent().next(".beschrijving").children().replaceWith("<form><input class='invoerveld' type='email'></form>");
+        $('input').focus();
+        $(".invoerveld").blur(function(){
+                  var waarde = $(".invoerveld").val();  
+                    console.log(waarde);
+        $(this).replaceWith("<p class='details'>" + waarde + "</p>");     
+        });
 
-
+});
 });
