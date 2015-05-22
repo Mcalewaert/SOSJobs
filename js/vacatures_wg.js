3e390519abb762dedad4dd34b0068e5e4d555bb0
JenteSwinnen <jente.swinnen@gmail.com>
2015-05-22 14:55:16 +0200

    odijxdo

--- js/vacatures_wg.js
+++ js/vacatures_wg.js
@@ -2,107 +2,66 @@ $(function(){
     
     $.mobile.loading().hide();
     
-    var animationDone = true;
+    var isTouchMoving = false;
     
-    var oldLink = "";
     var $vacature = $('#list li');
     var $vacatureLink = $('#list li .sliderUnder');
+    
+    $vacatureLink.on('tap',function(e){
+        console.log('vacature link geklikt');
+        console.log(isTouchMoving);
+        if(isTouchMoving){
+            isTouchMoving = false;
+            return false;
+        }
+        
+        window.location.href = "vacaturedetail.html";
+        
+    });
+    
+    
+    
     var $listItemIcons = $('.vacature .sliderOnTop .iconList');
     
-    $vacature.find('.vacature').css({right:'0'});
-    
-    $vacatureLink.on('tap', function(e){
+    // SWIPE LEFT
+    $vacature.on("swipeleft", function(e){
+        isTouchMoving = true;
         
-        e.preventDefault();
-        
-        var link = $(this).find('.linkToDetail[href]').attr('href');
-        
-        if(link != "#"){
-            
-            console.log('CHANGE PAGE TO: ' + link );
-            
-            //window.location.href = link;
-            
-        }
-        else{
-            console.log('Link disabled');
-        }
-        
-    });
-    
-    // SWIPE
-    //  left
-    $vacature.on('swipeleft', function(e){
-        
-        // haal oude link op uit html
-        oldLink = $(this).find('.linkToDetail[href]').attr('href');
-        
-        //plaats oude link in nieuwe attribuut
-        $(this).find('.linkToDetail[href]').attr('hrefold',oldLink);
-        
-        // maak link in html leeg, disable
-        $(this).find('.linkToDetail[href]').attr('href', "#");
-        
-        console.log('SWIPED LEFT');
-        
-        
-        
-        var alreadyOpenTest = $(this).css('right');
+        var alreadyOpenTest = $(this).find('.vacature').css('right');
         alreadyOpenTest = alreadyOpenTest.substring(0,alreadyOpenTest.length-2);
         
-        
-        // TEST IF VAC IS ALREADY SWIPED OPEN
-        if(alreadyOpenTest>0){
-            $(this).animate({right:'+=1em'},100,'linear',function(){
-                $(this).animate({right:'-=1em'},500,'easeOutElastic',function(){
-                    
-                    
-                    
-                });
+        if(alreadyOpenTest!=0){
+            // do nothing
+            isTouchMoving = false;
+            return false;
+        }
+        else{
+            // RESET ALL ORIGINAL POSITIONS
+            $vacature.animate({right:'0'},200,'easeInOutQuad');
+            $listItemIcons.animate({opacity:'0'},100,'linear');
+
+            // MOVE SWIPED ITEM
+            $listItemIcons.animate({opacity:'1'},100,'linear');
+            $(this).animate({right:'+=12em'},200,'easeInOutQuad',function(){
+                isTouchMoving = false;
+                return false;
             });
-            return false;
         }
-        
-        // RESET ORIGINAL POSITIONS
-        $vacature.animate({right:'0'},200,'easeInOutQuad');
-        $listItemIcons.animate({opacity:'0'},100,'linear');
-        
-        // MOVE SWIPED ITEMS
-        $listItemIcons.animate({opacity:'1'},100,'linear');
-        $(this).animate({right:'+=12em'},200,'easeInOutQuad');
-        
         return false;
-        
     });
-    //  right
-    $vacature.on('swiperight', function(e){
-        
+    
+    //SWIPE RIGHT
+    $vacature.on("swiperight", function(e){
+        isTouchMoving = true;
         var alreadyOpenTest = $(this).css('right');
         alreadyOpenTest = alreadyOpenTest.substring(0,alreadyOpenTest.length-2);
         
-        if(alreadyOpenTest>0){
-            $(this).animate({right:'0'},100,'easeInOutQuad');
-            $(this).find('.vacature .sliderOnTop .iconList').animate({opacity:'0'},100,'linear');
-            
-            oldLink = $(this).find('.linkToDetail[href]').attr('hrefold');
-            $(this).find('.linkToDetail[href="#"]').attr('href',oldLink);
-            console.log("SWIPED RIGHT");
-            return false;
+        if(alreadyOpenTest!=0){
+            $vacature.animate({right:'0'},100,'easeInOutQuad');
+            $listItemIcons.animate({opacity:'0'},100,'linear');
         }
-        else{
-            
-            //if(animationDone){
-                
-                $(this).animate({right:'-=1em'},100,'linear',function(){
-                    $(this).animate({right:'0'},500,'easeOutElastic',function(){
-                        //animationDone = true;
-                    });
-                });
-            //}
-        }
-        
+        isTouchMoving = false;
         return false;
-        
     });
     
     
