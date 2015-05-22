ed4760204808c43aa30973f52750b34a998cad34
JenteSwinnen <jente.swinnen@gmail.com>
2015-05-22 09:00:25 +0200

    groepen

---
+++ js/groepen.js
@@ -0,0 +1,8 @@
+    $(document).ready(function () {
+    
+    if($(".evenTeller").children(".groepen").length % 2 != 0){
+    
+    $(".groepen:last").css({"float": "none","margin": "auto","clear": "both"});
+    }
+       
+        });
